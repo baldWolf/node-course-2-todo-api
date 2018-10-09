d6f6c0812eb70136af7362e44cd6d7d793e020fd
lowolfne <lowolfnest@gmail.com>
2018-10-09 11:59:23 +0800

    add GET /users/me and authentication middleware

---
+++ server/middleware/authenticate.js
@@ -0,0 +1,24 @@
+var {user} = require('./../models/user');
+
+var authenticate = (req, res, next) => {
+    var token = req.header('x-auth');
+
+    user.findByToken(token).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        req.user = user;
+        req.token = token;
+        // run the next code
+        next();
+
+    }).catch((e) => {
+        res
+        .status(401)
+        .send();
+    });
+};
+
+
+module.exports = {authenticate};
--- server/server.js
+++ server/server.js
@@ -17,6 +17,8 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {user} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
 var app = express();
 const port = process.env.PORT; //|| 3000;
 
@@ -44,6 +46,27 @@ app.post('/users', (req,res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    
+    res.send(req.user);
+    // var token = req.header('x-auth');
+
+    // user.findByToken(token).then((user) => {
+    //     if (!user) {
+    //         return Promise.reject();
+    //     }
+
+    //     res.send(user);
+    // }).catch((e) => {
+    //     res
+    //     .status(401)
+    //     .send();
+    // });
 });
 
 // POST /todos
